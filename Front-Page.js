cc426e4ba7414a61bd2cc0f9d45766670f621be9
William Huang <williamhuang9611@gmail.com>
2025-08-13 23:59:13 -0400

    added fetch

--- Front-Page.js
+++ Front-Page.js
@@ -1,14 +1,4 @@
-var tuition = 0;
-var location = 0;
-var living = 0;
-var co_op = 0;
-var food = 0;
-var extra = 0;
-
-var unilist = ['McGill University','University of Toronto','University of British Columbia','Univeristy of Alberta','Western University','Université de Montréal','McMaster University','Queens Univeristy in Kingston','University of Calgary','University of Waterloo'];
-
-
 fetch("UniList.json")
     .then(response => response.json())
     .then(value => console.log(value))
-    .catch(error => console.error(error));
+    .catch(error => console.error(error)); 
