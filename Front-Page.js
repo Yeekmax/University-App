d7b67a80e9c2335ba7cccfd6dc13d0380cf971b5
William Huang <williamhuang9611@gmail.com>
2025-08-13 21:44:48 -0400

    Made 2 pages, still need images, wondering if its really worth it to make a page for each university

---
+++ Front-Page.js
@@ -0,0 +1,8 @@
+var tuition = 0;
+var location = 0;
+var living = 0;
+var co_op = 0;
+var food = 0;
+var extra = 0;
+
+var unilist = ['McGill University','University of Toronto','University of British Columbia','Univeristy of Alberta','Western University','Université de Montréal','McMaster University','Queens Univeristy in Kingston','University of Calgary','University of Waterloo'];
